1bdf923c62f99e82380088ecc64713abbcfc1501
Nagy Gergely <nagy.gergely14@gmail.com>
2017-03-17 09:29:37 +0100

    Added start and end events

--- ng-mousedrag.js
+++ ng-mousedrag.js
@@ -9,28 +9,44 @@
   ngMousedrag.directive('ngMousedrag', ['$document', function ngMousedrag($document) {
     return {
       restrict: 'A',
-      link: function (scope, element, attrs) {
-        var endTypes = 'touchend mouseup'
-          , moveTypes = 'touchmove mousemove'
-          , startTypes = 'touchstart mousedown'
-          , startX, startY;
+      link: function(scope, element, attrs) {
+        var endTypes = 'touchend mouseup',
+          moveTypes = 'touchmove mousemove',
+          startTypes = 'touchstart mousedown',
+          startX, startY, endX, endY;
 
         element.bind(startTypes, function startDrag(e) {
           e.preventDefault();
           startX = e.pageX;
           startY = e.pageY;
+          e.startX = startX;
+          e.startY = startY;
+          e.type = 'start';
+          scope.$event = e;
+          scope.$eval(attrs.ngMousedrag);
 
-          $document.bind(moveTypes, function (e) {
+          $document.bind(moveTypes, function(e) {
             e.dragX = e.pageX - startX;
             e.dragY = e.pageY - startY;
             e.startX = startX;
             e.startY = startY;
+            endX = e.dragX;
+            endY = e.dragY;
+            e.type = 'move';
             scope.$event = e;
             scope.$eval(attrs.ngMousedrag);
           });
 
-          $document.bind(endTypes, function () {
+          $document.bind(endTypes, function() {
             $document.unbind(moveTypes);
+            var e = {};
+            e.startX = startX;
+            e.startY = startY;
+            e.endX = endX;
+            e.endY = endY;
+            e.type = 'end';
+            scope.$event = e;
+            scope.$eval(attrs.ngMousedrag);
           });
         });
       }
